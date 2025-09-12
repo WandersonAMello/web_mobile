63e29fdf1cae226f70456483083631e81711319d
WandersonAMello <wandersonalmeidamello@gmail.com>
2025-09-11 21:45:17 -0300

    aula 11 de setembro

--- sistema/sistema/views.py
+++ sistema/sistema/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
 
 class Login(View):
     """
@@ -10,3 +11,20 @@ class Login(View):
     def get(self, request):
         contexto = {}
         return render(request, 'autenticacao.html', contexto)
+    
+    def post(self, request):
+        
+        # Obtém as credenciais do formulário
+        usuario = request.POST.get('usuario', None)
+        senha = request.POST.get('senha', None)
+        
+        # Verifica se as credenciais são válidas
+        user = authenticate(request, username=usuario, password=senha)
+        if user is not None:
+            
+            #verifica se o usuário está ativo no sistema
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Usuário autenticado com sucesso')
+            
+        return render(request, 'autenticacao.html', {'mensagem': 'Login invalido!'})
---
+++ sistema/veiculo/consts.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+OPCOES_MARCAS = (
+    (1, 'AUDI'),
+    (2, 'BMW'),
+    (3, 'CHEVROLET - GM'),
+    (4, 'FIAT'),
+    (5, 'FORD'),
+    (6, 'HONDA'),
+    (7, 'HYUNDAI'),
+    (8, 'VOLKSWAGEN'),
+)
+
+OPCOES_CORES = (
+    (1, 'BRANCO'),
+    (2, 'AMARELO'),
+    (3, 'AZUL'),
+    (4, 'PRATA'),
+    (5, 'PRETO'),
+    (6, 'VERMELHO'),
+)
+
+OPCOES_COMBUSTIVEIS = (
+    (1, 'ETANOL'),
+    (2, 'DIESEL'),
+    (3, 'FLEX'),
+    (4, 'GASOLINA'),
+)
