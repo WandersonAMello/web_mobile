fbdc420d945cc76d1698afc55f7469fe1f29b4c9
WandersonAMello <wandersonalmeidamello@gmail.com>
2025-11-06 19:52:43 -0300

    erro de cors

--- sistema/veiculo/serializers.py
+++ sistema/veiculo/serializers.py
@@ -6,6 +6,21 @@ class SerializadorVeiculo(serializers.ModelSerializer):
     Serializador para o model Veiculo
     
     """
+    
+    nome_marca = serializers.SerializerMethodField()
+    nome_cor = serializers.SerializerMethodField()
+    nome_combustivel = serializers.SerializerMethodField()
+    
+    
     class Meta:
         model = Veiculo
         exclude = []
+        
+    def get_nome_marca(self, instancia):
+        return instancia.get_marca_display()
+    
+    def get_nome_cor(self, instancia):
+        return instancia.get_cor_display()
+    
+    def get_nome_combustivel(self, instancia):
+        return instancia.get_combustivel_display()
